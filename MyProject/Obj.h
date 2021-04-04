9ae336e2dbae8f2c9e4f84a0f28b8b7430761558
sunkue <wmwwmmm@naver.com>
2021-04-04 16:18:20 +0900

    chapter 2. 기본구조 수정.

--- MyProject/Obj.h
+++ MyProject/Obj.h
@@ -2,76 +2,93 @@
 
 #include "pch.h"
 
-
+/* 버텍스에 담을 정보들.. col 값은 사실상 단색 tex 로 교체 가능함.. */
 struct Vertex
 {
-	m_vec3 pos;
-	m_vec3 col;
-	m_vec3 nor;
-	m_vec3 tex;
+	XMFLOAT3		pos;
+	XMFLOAT3		col;
+	XMFLOAT3		nor;
+	XMFLOAT3		tex;
 };
 
+
+/* 월드에 존재하는 모든 것들. 월드변환 행렬 */
+__interface IWORLDMAT
+{
+	virtual XMMATRIX		WorldMat() abstract;
+};
+class WORLDOBJ abstract : public IWORLDMAT
+{
+private:
+	XMFLOAT4X4		m_translateMat{ BASICMAT };
+	XMFLOAT4X4		m_roatateMat{ BASICMAT };
+	XMFLOAT4X4		m_scaleMat{ BASICMAT };
+};
+
+
+
+
+/* 파티클을 이루는 요소한개 */
 class Particle
 {
 public:
-	Particle(vector<Vertex>&& vx, m_vec3 vel = { 0.f,0.f,0.f }) :m_vertices{ move(vx) }, m_velocity{ vel }, m_emitTime{}{}
-	Particle(vector<Vertex>&& vx, Fvec vel) :m_vertices{ move(vx) }, m_emitTime{}{V2M3(&m_velocity, vel); }
+	//Particle(vector<Vertex>&& vx, XMFLOAT3 vel = { 0.f,0.f,0.f }) :m_vertices{ vx }, m_velocity{ vel }, m_emitTime{}{}
+	//Particle(vector<Vertex>&& vx, FXMVECTOR vel) :m_vertices{ vx }, m_emitTime{}{DirectX::XMStoreFloat3(&m_velocity, vel); }
+	Particle(vector<Vertex>& vx, XMFLOAT3 vel = { 0.f,0.f,0.f }) :mp_vertices{ make_shared<vector<Vertex>>(vx) }, m_velocity{ vel }, m_emitTime{}{}
+	Particle(vector<Vertex>& vx, FXMVECTOR vel) :mp_vertices{ make_shared<vector<Vertex>>(vx) }, m_emitTime{}{XMStoreFloat3(&m_velocity, vel); }
 
-	float	lifeTime() { return static_cast<std::chrono::duration<float, std::milli>>(clk::now() - m_emitTime).count(); }
+	float	LifeTime() { return static_cast<std::chrono::duration<float, std::milli>>(clk::now() - m_emitTime).count(); }
 	void	born() { m_emitTime = clk::now(); }
 private:
-	vector<Vertex>	m_vertices;
-	m_vec3			m_velocity;
+	shared_ptr<vector<Vertex>>	mp_vertices;
+	XMFLOAT3		m_velocity;
 	tp				m_emitTime;
-	int f() {
-		int a;
-		vector<char>v;
-		M2V2(v);
-	}
 };
 
-class Particles
+/* 파티클 */
+class Particles : public WORLDOBJ
 {
 public:
-	size_t GetNumOfParticles() { return m_particles.size(); }
+	Particles(vector<Particle>& ptcl, XMFLOAT3 vel = { 0.f,0.f,0.f }) :mp_particles{ make_shared<vector<Particle>>(ptcl) }, m_velocity{ vel }{}
+	Particles(vector<Particle>& ptcl, FXMVECTOR vel) :mp_particles{ make_shared<vector<Particle>>(ptcl) } { XMStoreFloat3(&m_velocity, vel); }
+	size_t			GetNumOfParticles() { return mp_particles->size(); }
+
+	XMMATRIX		WorldMat() final;
 private:
-	vector<Particle> m_particles;
-	float m_translateMat4x4[4][4];
-	float m_roatateMat4x4[4][4];
-	float m_scaleMat4x4[4][4];
+	shared_ptr<vector<Particle>>mp_particles;
+	XMFLOAT3		m_velocity;
 };
 
-class ObjPart
+
+
+
+/* 오브젝트 부위 */
+class ObjPart : public WORLDOBJ
 {
 public:
-	ObjPart(vector<Vertex>&& vx, m_vec3 vel = { 0.f,0.f,0.f }) :m_vertices{ move(vx) }, m_velocity{ vel }{}
-	ObjPart(vector<Vertex>&& vx, Fvec vel) :m_vertices{ move(vx) } { V2M3(&m_velocity, vel); }
+	//ObjPart(vector<Vertex>&& vx, XMFLOAT3 vel = { 0.f,0.f,0.f }) :m_vertices{ vx }, m_velocity{ vel }{}
+	//ObjPart(vector<Vertex>&& vx, FXMVECTOR vel) :m_vertices{ vx } { DirectX::XMStoreFloat3(&m_velocity, vel); }
+	ObjPart(vector<Vertex>& vx, XMFLOAT3 vel = { 0.f,0.f,0.f }) :mp_vertices{ make_shared<vector<Vertex>>(vx) }, m_velocity{ vel }{}
+	ObjPart(vector<Vertex>& vx, FXMVECTOR vel) :mp_vertices{ make_shared<vector<Vertex>>(vx) } { XMStoreFloat3(&m_velocity, vel); }
 
-	//float* m_worldMat4x4() { return m_scaleMat4x4 * m_roatateMat4x4 * m_translateMat4x4; }
-	//다렉 매쓰 매트릭스로 바꿀 것임.
+	XMMATRIX		WorldMat() final;
 private:
-	vector<Vertex>	m_vertices;
-	m_vec3 m_velocity;
-
-	float m_translateMat4x4[4][4];
-	float m_roatateMat4x4[4][4];
-	float m_scaleMat4x4[4][4];
-
-	index m_texture;
+	shared_ptr<vector<Vertex>>	mp_vertices;
+	index			m_texture;
+	XMFLOAT3		m_velocity;
 };
 
-class Obj
+/* 부위 통합된 오브젝트 */
+class Obj : public WORLDOBJ
 {
 public:
-	Obj(vector<ObjPart>&& objParts, m_vec3 vel = { 0.f,0.f,0.f }) :m_objPart{ move(objParts) }, m_velocity{ vel } {}
-	Obj(vector<ObjPart>&& objParts, Fvec vel) :m_objPart{ move(objParts) } { V2M3(&m_velocity, vel); }
+	//Obj(vector<ObjPart>&& objParts, XMFLOAT3 vel = { 0.f,0.f,0.f }) :m_objPart{ (objParts) }, m_velocity{ vel } {}
+	//Obj(vector<ObjPart>&& objParts, FXMVECTOR vel) :m_objPart{ make_shared< (objParts) } { DirectX::XMStoreFloat3(&m_velocity, vel); }
+	Obj(vector<ObjPart>& objParts, XMFLOAT3 vel = { 0.f,0.f,0.f }) :mp_objPart{ make_shared< vector<ObjPart>>(objParts) }, m_velocity{ vel } {}
+	Obj(vector<ObjPart>& objParts, FXMVECTOR vel) :mp_objPart{ make_shared< vector<ObjPart>>(objParts) } { XMStoreFloat3(&m_velocity, vel); }
 
-	//float* m_worldMat4x4(); //part 전부에 적용
+	XMMATRIX		WorldMat() final;
 private:
-	vector<ObjPart>	m_objPart;
-	m_vec3 m_velocity;
-
-	float m_translateMat4x4[4][4];
-	float m_roatateMat4x4[4][4];
-	float m_scaleMat4x4[4][4];
+	shared_ptr<vector<ObjPart>>	mp_objPart;
+	XMFLOAT3		m_velocity;
 };
--- MyProject/pch.h
+++ MyProject/pch.h
@@ -41,81 +41,31 @@ using	index = unsigned int;
 /*
 		idea :
 			XMVECTOR 타입을 통하여 SIMD 명령을 사용해야한다.
+			데이터는 XMVECTOR XMFLOAT(멤버변수) XMMATRIX(XMVECTOR[4]) 등을 사용.
 			sse 레지스터의 활용을 위해, 함수 안에서 계산에 사용할 파라미터 XMVECTOR의 타입을 지정하자. [ 123F 4G 56H etcC ] 함수이름앞에 XM_CALLCONV 지시자 붙이기
 				생성자는 [ 123F etcC ] 지시자 X
-
-		타입이름 규칙 :
-			XMVECTOR 는 메모리의 연속성(alignment)이 보장되어야 한다. 지역, 전역변수로 사용시 보장됨 자주 쓸 것이므로 읽기쓰기 편한 vec으로 했음.
-			XMFLOAT 류는 메모리의 연속성이 보장되지 않는 클래수 멤버에서 쓸 것이므로 타입이름에 m_을 붙여구분하였음.
-
-		함수이름 규칙 :
-			시작 글자가 V => XMVECTOR 가 주체.
-			시작 글자가 M => XMVFLOAT 가 주체.
-
-		edited :	4 / 03
+		edited :	4 / 04
 */
 
 #include <DirectXMath.h>
 #include <DirectXPackedVector.h>
-using	m_vec2 = DirectX::XMFLOAT2;	// 이름 후보 : m_vec2 MVEC2 MV2 vec2 mvec2 Mvec2 XMF2
-using	m_vec3 = DirectX::XMFLOAT3;
-using	m_vec4 = DirectX::XMFLOAT4;
-using	vec = DirectX::XMVECTOR;	// 이름 후보 : vec VEC V XMV
-using	Fvec = DirectX::FXMVECTOR;
-using	Gvec = DirectX::GXMVECTOR;
-using	Hvec = DirectX::HXMVECTOR;
-using	Cvec = DirectX::CXMVECTOR;
-using	CONSTvec = DirectX::XMVECTORF32;
+
 using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+constexpr XMFLOAT4X4 BASICMAT{	1.f,0.f,0.f,0.f,
+								0.f,1.f,0.f,0.f,
+								0.f,0.f,1.f,0.f,
+								0.f,0.f,0.f,1.f };
+
+inline bool operator==(FXMVECTOR V1, FXMVECTOR V2) { return DirectX::XMVector3Equal(V1, V2); }
+inline bool operator!=(FXMVECTOR V1, FXMVECTOR V2) { return !(V1 == V2); }
+
+
+
+
 /*
-		auto, ...Args 등으로 맹글면 컴파일 전까지 오류 확인 안됨! 손 아파도 타입 죄다 적자...
-		c++ 20 표준 생기믄 이걸로 가자
-		inlnie auto myf(auto a){return f(a);}
-		template<typename...Args> inline auto myf(Args&&... args) { return f(std::forward<Args>(args)...); }
-
-		굳이 이름을 바꾸어야 할까? 라는 고민...
-		하다보면 손에익지 않을까? 라는 생각..
-*/
-
-inline auto	XM_CALLCONV M2V2(const m_vec2* pSource) { return DirectX::XMLoadFloat2(pSource); }
-inline auto	XM_CALLCONV M2V3(const m_vec3* pSource) { return DirectX::XMLoadFloat3(pSource); }
-inline auto	XM_CALLCONV M2V4(const m_vec4* pSource) { return DirectX::XMLoadFloat4(pSource); }
-inline auto	XM_CALLCONV V2M2(m_vec2* pDestination, Fvec V) { return DirectX::XMStoreFloat2(pDestination, V); }
-inline auto	XM_CALLCONV V2M3(m_vec3* pDestination, Fvec V) { return DirectX::XMStoreFloat3(pDestination, V); }
-inline auto	XM_CALLCONV V2M4(m_vec4* pDestination, Fvec V) { return DirectX::XMStoreFloat4(pDestination, V); }
-inline auto XM_CALLCONV VGetX(Fvec V) { return DirectX::XMVectorGetX(V); }
-inline auto XM_CALLCONV VGetY(Fvec V) { return DirectX::XMVectorGetY(V); }
-inline auto XM_CALLCONV VGetZ(Fvec V) { return DirectX::XMVectorGetZ(V); }
-inline auto XM_CALLCONV VGetW(Fvec V) { return DirectX::XMVectorGetW(V); }
-inline auto XM_CALLCONV VSetX(Fvec V, float x) { return DirectX::XMVectorSetX(V, x); }
-inline auto XM_CALLCONV VSetY(Fvec V, float y) { return DirectX::XMVectorSetY(V, y); }
-inline auto XM_CALLCONV VSetZ(Fvec V, float z) { return DirectX::XMVectorSetZ(V, z); }
-inline auto XM_CALLCONV VSetW(Fvec V, float w) { return DirectX::XMVectorSetW(V, w); }
-inline auto Radians(float fDegrees) { return DirectX::XMConvertToRadians(fDegrees); }
-inline auto Degrees(float fRadians) { return DirectX::XMConvertToDegrees(fRadians); }
-inline auto XM_CALLCONV MakeVecZero() { return DirectX::XMVectorZero(); }
-inline auto XM_CALLCONV MakeVecOne() { return DirectX::XMVectorSplatOne(); }
-inline auto XM_CALLCONV MakeVec(float x, float y, float z, float w) { return DirectX::XMVectorSet(x, y, z, w); }
-inline auto XM_CALLCONV MakeVecWith(float value) { return DirectX::XMVectorReplicate(value); }
-inline auto XM_CALLCONV MakeVecWithX(Fvec V) { return DirectX::XMVectorSplatX(V); }
-inline auto XM_CALLCONV MakeVecWithY(Fvec V) { return DirectX::XMVectorSplatY(V); }
-inline auto XM_CALLCONV MakeVecWithZ(Fvec V) { return DirectX::XMVectorSplatZ(V); }
-inline auto XM_CALLCONV MakeVecWithW(Fvec V) { return DirectX::XMVectorSplatW(V); }
-inline auto XM_CALLCONV Length(Fvec V) { return DirectX::XMVector3Length(V); }
-inline auto XM_CALLCONV LengthSq(Fvec V) { return DirectX::XMVector3LengthSq(V); }
-inline auto XM_CALLCONV Dot(Fvec V1, Fvec V2) { return DirectX::XMVector3Dot(V1, V2); }
-inline auto XM_CALLCONV Cross(Fvec V1, Fvec V2) { return DirectX::XMVector3Cross(V1, V2); }
-inline auto XM_CALLCONV Normalize(Fvec V) { return DirectX::XMVector3Normalize(V); }
-inline auto XM_CALLCONV Orthogonal(Fvec V) { return DirectX::XMVector3Orthogonal(V); }
-inline auto XM_CALLCONV RadiansBetweenVecs(Fvec V1, Fvec V2) { return DirectX::XMVector3AngleBetweenVectors(V1, V2); }
-inline auto XM_CALLCONV ComponentsFromNormal(vec* pParallel, vec* pPerpendcular, Fvec V, Fvec Normal) { return DirectX::XMVector3ComponentsFromNormal(pParallel, pPerpendcular, V, Normal); }
-
-const DirectX::XMVECTOR Epsilon{ MakeVecWith(0.00000005f) };
-
-inline bool operator==(Fvec V1, Fvec V2) { return DirectX::XMVector3NearEqual(V1, V2, Epsilon); }
-inline bool operator!=(Fvec V1, Fvec V2) { return !(V1 == V2); }
-
-
-
-
-
+	[ 독파 진행상황 ]
+	2	/	23 챕터
+	1주일에 3개씩 -> 8주 정복. 두달.
+*/	
